acc1dfe9af032143f3b0fdbe2872906f883b61b6
Guy Twa <gtwa@uab.edu>
2024-08-24 15:19:48 -0500

    Editing example function call to be multi-line

--- R/create_pseudobulk.R
+++ R/create_pseudobulk.R
@@ -16,7 +16,10 @@
 #' @export
 #'
 #' @examples
-#' create_pseudobulk(seurat_obj = Neurons, cell_type_var = "CellType", grouping_vars = c("Sex","GEM"), split = TRUE)
+#' create_pseudobulk(seurat_obj = Neurons,
+#'   cell_type_var = "CellType",
+#'   grouping_vars = c("Sex","GEM"),
+#'   split = TRUE)
 
 create_pseudobulk <- function(seurat_obj, cell_type_var, grouping_vars, split = FALSE){
   cts <- AggregateExpression(object = seurat_obj,
