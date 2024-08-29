6fd299a6720117f4238837fa81472d3da63666f4
Guy Twa <gtwa@uab.edu>
2024-08-29 08:46:14 -0500

    Modified results_DESeq.
    Added argument for cell type metadata column and cell type level being evaluated for current results table. Corrects the pct_Ecpressing columns showing NA for cells.1, and assignment of values to celltype column. Now this can be called with an imap() function with a list of fit DESeq objects named by cell type.

--- R/results_DESeq.R
+++ R/results_DESeq.R
@@ -20,41 +20,43 @@
 #' #   Contrast = c("Treatment", "Mor", "Sal")
 #' # )
 #'
-results_DESeq <- function(DESeq_obj, seurat_obj, Contrast) {
-    # Extract results
-    res <- results(DESeq_obj, contrast = Contrast) %>%
-        data.frame() %>%
-        rownames_to_column(var = "GeneName")
-    # Extract counts table
-    counts_mat <- seurat_obj@assays[["RNA"]]@counts
-    # Replace any hyphens in gene names with ".", to match results table
-    rownames(counts_mat) <- gsub("-", ".", rownames(counts_mat))
-    # DESeq2 will prepend "X" to genes with names that start with numbers
-    # consequently, this mucks up a bunch of stuff so we'll get ahead of it here
-    # Identify genes that start with a number
-    numeric_genes <- grep("^[0-9]", rownames(counts_mat), value = TRUE)
-    # modify the corresponding gene names in DESeq2 results
-    modified_gene_names <- paste0("X", numeric_genes)
-    # create a mapping between the modified and original gene names
-    gene_mapping <- setNames(numeric_genes, modified_gene_names)
-    res$GeneName <- ifelse(res$GeneName %in% names(gene_mapping),
-        gene_mapping[res$GeneName],
-        res$GeneName
+results_DESeq <- function(DESeq_obj, seurat_obj, Contrast, cell_type_var, cell_type_level) {
+  # Extract results
+  res <- results(DESeq_obj, contrast = Contrast) %>%
+    data.frame() %>%
+    rownames_to_column(var = "GeneName")
+  # Extract counts table
+  counts_mat <- seurat_obj@assays[["RNA"]]@counts
+  # Replace any hyphens in gene names with ".", to match results table
+  rownames(counts_mat) <- gsub("-", ".", rownames(counts_mat))
+  # DESeq2 will prepend "X" to genes with names that start with numbers
+  # consequently, this mucks up a bunch of stuff so we'll get ahead of it here
+  # Identify genes that start with a number
+  numeric_genes <- grep("^[0-9]", rownames(counts_mat), value = TRUE)
+  # modify the corresponding gene names in DESeq2 results
+  modified_gene_names <- paste0("X", numeric_genes)
+  # create a mapping between the modified and original gene names
+  gene_mapping <- setNames(numeric_genes, modified_gene_names)
+  res$GeneName <- ifelse(res$GeneName %in% names(gene_mapping),
+                         gene_mapping[res$GeneName],
+                         res$GeneName
+  )
+  # Format results table
+  cells.1 <- seurat_obj@meta.data %>%
+    filter(!!sym(cell_type_var) == cell_type_level) %>%
+    filter(!!sym(Contrast[1]) == Contrast[2]) %>%
+    row.names()
+  cells.2 <- setdiff(x = seurat_obj@meta.data %>%
+                       filter(!!sym(cell_type_var) == cell_type_level) %>%
+                       row.names(),
+                     y = cells.1)
+  res$Pct_Expressing <- (rowSums(x = counts_mat[res$GeneName, cells.1, drop = FALSE] > 0) / length(cells.1)) * 100
+  res$Pct_Other_Expressing <- (rowSums(x = counts_mat[res$GeneName, cells.2, drop = FALSE] > 0) / length(cells.2)) * 100
+  res$CellType <- cell_type_level
+  # order column names
+  res <- res %>%
+    select(CellType, GeneName, baseMean, log2FoldChange, lfcSE, stat, pvalue,
+           padj, Pct_Expressing, Pct_Other_Expressing
     )
-    # Format results table
-    cells.1 <- seurat_obj@meta.data %>%
-        filter(Contrast[1] == Contrast[2]) %>%
-        row.names()
-    cells.2 <- setdiff(x = row.names(seurat_obj@meta.data), y = cells.1)
-    res$Pct_Expressing <- (rowSums(x = counts_mat[res$GeneName, cells.1, drop = FALSE] > 0) / length(cells.1)) * 100
-    res$Pct_Other_Expressing <- (rowSums(x = counts_mat[res$GeneName, cells.2, drop = FALSE] > 0) / length(cells.2)) * 100
-    res$CellType <- CellType
-    # order column names
-    res <- res %>%
-        select(
-            CellType, GeneName, baseMean, log2FoldChange, lfcSE, stat, pvalue,
-            padj, Pct_CellType_Expressing, Pct_Other_Expressing
-        )
-
-    return(res)
+  return(res)
 }
