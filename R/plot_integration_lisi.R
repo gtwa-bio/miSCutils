429d9741e31755a59c56680c5673ef777f3c2ba5
Guy Twa <gtwa@uab.edu>
2024-09-27 10:42:09 -0500

    Added functions for integration metric plotting `plot_integration_bar()` and `plot_integration_lisi()`. Additional formatting and updates to NAMESPACE and manuals to incorporate these new functions and their documentation

--- R/plot_integration_lisi.R
+++ R/plot_integration_lisi.R
@@ -18,59 +18,81 @@
 #' @export
 #'
 #' @examples
-plot_integration_lisi <- function(seurat_obj, cluster_col, treatment_col, gem_col, sex_col, reduction = "umap"){
-  ## take an integrated Seurat object, plot distributions of LISI index for treatment group, GEM well, and predicted sex
+plot_integration_lisi <- function(seurat_obj, cluster_col, treatment_col, gem_col, sex_col, reduction = "umap") {
+    ## take an integrated Seurat object, plot distributions of LISI index for treatment group, GEM well, and predicted sex
 
-  # Calculate for groups
-  ## Treatment group
-  seurat_obj@meta.data$Treatment.LISI <- compute_lisi(X = seurat_obj@reductions[[reduction]]@cell.embeddings,
-                                                meta_data = seurat_obj@meta.data,
-                                                label_colnames = treatment_col)[[treatment_col]]
+    # Calculate for groups
+    ## Treatment group
+    seurat_obj@meta.data$Treatment.LISI <- compute_lisi(
+        X = seurat_obj@reductions[[reduction]]@cell.embeddings,
+        meta_data = seurat_obj@meta.data,
+        label_colnames = treatment_col
+    )[[treatment_col]]
 
-  ## GEM well
-  seurat_obj@meta.data$GEM.LISI <- compute_lisi(X = seurat_obj@reductions[[reduction]]@cell.embeddings,
-                                          meta_data = seurat_obj@meta.data,
-                                          label_colnames = gem_col)[[gem_col]]
+    ## GEM well
+    seurat_obj@meta.data$GEM.LISI <- compute_lisi(
+        X = seurat_obj@reductions[[reduction]]@cell.embeddings,
+        meta_data = seurat_obj@meta.data,
+        label_colnames = gem_col
+    )[[gem_col]]
 
-  ## predicted sex
-  seurat_obj@meta.data$Sex.LISI <- compute_lisi(X = seurat_obj@reductions[[reduction]]@cell.embeddings,
-                                          meta_data = seurat_obj@meta.data,
-                                          label_colnames = sex_col)[[sex_col]]
+    ## predicted sex
+    seurat_obj@meta.data$Sex.LISI <- compute_lisi(
+        X = seurat_obj@reductions[[reduction]]@cell.embeddings,
+        meta_data = seurat_obj@meta.data,
+        label_colnames = sex_col
+    )[[sex_col]]
 
-  # Plot ridges
-  Treatment_lisi_ridge <- ggplot(seurat_obj@meta.data,
-                                 aes(x = Treatment.LISI,
-                                     y = !!sym(cluster_col),
-                                     fill = !!sym(cluster_col))) +
-    theme_bw() +
-    ggridges::geom_density_ridges() +
-    labs(x = "LISI", y = "Cluster", title = "Treatment Group") +
-    NoLegend()
+    # Plot ridges
+    Treatment_lisi_ridge <- ggplot(
+        seurat_obj@meta.data,
+        aes(
+            x = Treatment.LISI,
+            y = !!sym(cluster_col),
+            fill = !!sym(cluster_col)
+        )
+    ) +
+        theme_bw() +
+        ggridges::geom_density_ridges() +
+        labs(x = "LISI", y = "Cluster", title = "Treatment Group") +
+        NoLegend()
 
-  GEM_lisi_ridge <- ggplot(seurat_obj@meta.data,
-                           aes(x = GEM.LISI,
-                               y = !!sym(cluster_col),
-                               fill = !!sym(cluster_col))) +
-    ggridges::geom_density_ridges() +
-    theme_bw() +
-    labs(x = "LISI", y = "", title = "GEM Well") +
-    NoLegend() +
-    theme(axis.title.y = element_blank(),
-          axis.ticks.y=element_blank(),
-          axis.text.y=element_blank())
+    GEM_lisi_ridge <- ggplot(
+        seurat_obj@meta.data,
+        aes(
+            x = GEM.LISI,
+            y = !!sym(cluster_col),
+            fill = !!sym(cluster_col)
+        )
+    ) +
+        ggridges::geom_density_ridges() +
+        theme_bw() +
+        labs(x = "LISI", y = "", title = "GEM Well") +
+        NoLegend() +
+        theme(
+            axis.title.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank()
+        )
 
-  Sex_lisi_ridge <- ggplot(seurat_obj@meta.data,
-                           aes(x = Sex.LISI,
-                               y = !!sym(cluster_col),
-                               fill = !!sym(cluster_col))) +
-    ggridges::geom_density_ridges() +
-    theme_bw() +
-    labs(x = "LISI", y = "", title = "Predicted Sex") +
-    NoLegend() +
-    theme(axis.title.y = element_blank(),
-          axis.ticks.y=element_blank(),
-          axis.text.y=element_blank())
+    Sex_lisi_ridge <- ggplot(
+        seurat_obj@meta.data,
+        aes(
+            x = Sex.LISI,
+            y = !!sym(cluster_col),
+            fill = !!sym(cluster_col)
+        )
+    ) +
+        ggridges::geom_density_ridges() +
+        theme_bw() +
+        labs(x = "LISI", y = "", title = "Predicted Sex") +
+        NoLegend() +
+        theme(
+            axis.title.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank()
+        )
 
-  combined.plot <- Treatment_lisi_ridge + GEM_lisi_ridge + Sex_lisi_ridge + plot_layout(widths = c(3,3,3), heights = c(5,5,5), ncol = 3, nrow = 1)
-  return(combined.plot)
+    combined.plot <- Treatment_lisi_ridge + GEM_lisi_ridge + Sex_lisi_ridge + plot_layout(widths = c(3, 3, 3), heights = c(5, 5, 5), ncol = 3, nrow = 1)
+    return(combined.plot)
 }
